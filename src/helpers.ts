bd576fa3dce0e2b45279de139aa786d7ff1a0bc4
Your Name <yan.zaycev15@gmail.com>
2019-07-06 13:19:51 +0300

    Move idgenerator to helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -26,3 +26,18 @@ export const getNextMonthDaysToDisplay = (weekday: number): number => {
 
   return 7 - weekday;
 };
+
+export const idGenerator = (): Function => {
+  const ids: string[] = [];
+
+  return function generate(): string {
+    const newId: string = Math.random().toString(10).substring(2, 5) + Math.random().toString(10).substring(2, 5);
+
+    if (!ids.includes((newId))) {
+      ids.push(newId);
+      return newId;
+    }
+
+    return generate();
+  }
+};
