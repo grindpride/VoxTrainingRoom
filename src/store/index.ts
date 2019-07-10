c3acdbb35e579fa2cc22148475e8068cc647d134
YanZaytsev <yan.zaycev15@gmail.com>
2019-07-10 13:42:53 +0300

    Put mutations into separate file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue';
-import Vuex, {GetterTree, MutationTree} from 'vuex';
+import Vuex, {GetterTree, } from 'vuex';
 
-import {EventCoords, EventTimeInterval, State, TimeSlotsCoords} from "@/lib/types";
+import { State} from "@/lib/types";
 import {monthNames} from "@/lib/consts";
-import {createDefaultEvent, getCoordsByTime, getTimeByCoords} from "@/lib/helpers";
+import {createDefaultEvent} from "@/lib/helpers";
+import {mutations} from "@/store/mutations";
 
 Vue.use(Vuex);
 
@@ -28,48 +29,6 @@ const getters: GetterTree<State, any> = {
     }
 
     return [];
-  }
-};
-
-const mutations: MutationTree<State> = {
-  changeDate(state, newDate) {
-    state.activeDate = newDate;
-  },
-
-  addEvent(state, event) {
-    const dateStr: string = state.activeDate.toDateString();
-
-    if (!state.events.hasOwnProperty(dateStr)) {
-      state.events = {...state.events, [dateStr]: []}
-    }
-
-    state.events[dateStr].push(event);
-  },
-
-  resetEvent(state) {
-    state.currentEvent = createDefaultEvent();
-  },
-
-  setTimeSlotCoords(state, timeSlotCoords) {
-    state.timeSlotsCoords = timeSlotCoords;
-  },
-
-  setCoords(state, {startTime, endTime}) {
-    state.currentEvent.startTime = startTime;
-    state.currentEvent.endTime = endTime;
-
-    const {top, bottom}: EventCoords = getCoordsByTime(<TimeSlotsCoords[]>(state.timeSlotsCoords), {startTime, endTime});
-
-    state.currentEvent.styles.top = `${top}px`;
-    state.currentEvent.styles.height = `${bottom - top}px`;
-  },
-
-  setTimeInterval(state, { top, bottom}) {
-
-    const {startTime, endTime}: EventTimeInterval = getTimeByCoords(<TimeSlotsCoords[]>state.timeSlotsCoords, {top, bottom});
-
-    state.currentEvent.startTime = startTime;
-    state.currentEvent.endTime = endTime;
   }
 };
 
---
+++ src/store/mutations.ts
@@ -0,0 +1,45 @@
+import {MutationTree} from "vuex";
+import {EventCoords, EventTimeInterval, State, TimeSlotsCoords} from "@/lib/types";
+import {createDefaultEvent, getCoordsByTime, getTimeByCoords} from "@/lib/helpers";
+
+export const mutations: MutationTree<State> = {
+  changeDate(state, newDate) {
+    state.activeDate = newDate;
+  },
+
+  addEvent(state, event) {
+    const dateStr: string = state.activeDate.toDateString();
+
+    if (!state.events.hasOwnProperty(dateStr)) {
+      state.events = {...state.events, [dateStr]: []}
+    }
+
+    state.events[dateStr].push(event);
+  },
+
+  resetEvent(state) {
+    state.currentEvent = createDefaultEvent();
+  },
+
+  setTimeSlotCoords(state, timeSlotCoords) {
+    state.timeSlotsCoords = timeSlotCoords;
+  },
+
+  setCoords(state, {startTime, endTime}) {
+    state.currentEvent.startTime = startTime;
+    state.currentEvent.endTime = endTime;
+
+    const {top, bottom}: EventCoords = getCoordsByTime(<TimeSlotsCoords[]>(state.timeSlotsCoords), {startTime, endTime});
+
+    state.currentEvent.styles.top = `${top}px`;
+    state.currentEvent.styles.height = `${bottom - top}px`;
+  },
+
+  setTimeInterval(state, { top, bottom}) {
+
+    const {startTime, endTime}: EventTimeInterval = getTimeByCoords(<TimeSlotsCoords[]>state.timeSlotsCoords, {top, bottom});
+
+    state.currentEvent.startTime = startTime;
+    state.currentEvent.endTime = endTime;
+  }
+};
